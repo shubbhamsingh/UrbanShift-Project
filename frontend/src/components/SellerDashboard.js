b853fb5bc2d049b510a36916a8d7c04f8681bb2a
Shubham Raj <shubhamsingh161203@gmail.com>
2026-01-22 18:38:58 +0530

    Optimized AddProperty and SellerDashboard

--- frontend/src/components/SellerDashboard.js
+++ frontend/src/components/SellerDashboard.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios'; // Upload ke liye axios chahiye
 
 const SellerDashboard = () => {
   const navigate = useNavigate();
