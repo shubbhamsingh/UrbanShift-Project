6b490f2b7668cbef25154f62b85511906141779d
Shubham Raj <shubhamsingh161203@gmail.com>
2026-01-24 19:02:57 +0530

    Enabled PWA: Added Service Worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// 👇 1. Ye import add karein
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 👇 2. Ye line add karein (PWA Active karne ke liye)
+serviceWorkerRegistration.register(); 
+
 reportWebVitals();
